a37afcad332ac7f53db3ba00ef1b4717ea0ab097
Dritriy Golyshev <4dimariK@gmail.com>
2022-01-14 19:44:07 +0300

    add contacts module

---
+++ src/modules/contacts.js
@@ -0,0 +1,40 @@
+const contacts = () => {
+  const block = document.querySelector(".header-contacts");
+  const firstNumberBlock = block.querySelector(
+    ".header-contacts__phone-number-wrap"
+  );
+
+  const secondNumberBlock = block.querySelector(
+    ".header-contacts__phone-number-accord"
+  );
+  const arrowSelector = ".header-contacts__arrow";
+
+  const hiddenPhoneNumber = secondNumberBlock.querySelector(
+    ".header-contacts__phone-number"
+  );
+
+  const handlerSecondNumber = (show, top) => {
+    return show
+      ? { top, opacity: 1, transform: "rotate(180deg)" }
+      : { top: 0, opacity: 0, transform: "none" };
+  };
+
+  block.addEventListener("click", (e) => {
+    const { target } = e;
+
+    if (target.closest(arrowSelector)) {
+      const { height: firstNumberBlockHeight } =
+        getComputedStyle(firstNumberBlock);
+      const active = secondNumberBlock.classList.toggle("active");
+      const { top, opacity, transform } = handlerSecondNumber(
+        active,
+        firstNumberBlockHeight
+      );
+      target.style.transform = transform;
+      secondNumberBlock.style.top = top;
+      hiddenPhoneNumber.style.opacity = opacity;
+    }
+  });
+};
+
+module.exports = contacts;
